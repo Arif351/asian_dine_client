01d668a55652577b3a9a132dcc7117ff3461208d
Arif351 <ariful8836@gmail.com>
2022-12-26 13:00:43 +0600

    food items

--- src/Components/FoodMenu/EachFoodDetails.js
+++ src/Components/FoodMenu/EachFoodDetails.js
@@ -6,14 +6,16 @@ const EachFoodDetails = ({ eachFood }) => {
     console.log(see_more);
 
     return (
-        <div className='grid grid-cols-1  gap-4 lg:mx-8'>
-            {
-                see_more.map(allItems => <AllItems
-                    key={allItems.unid}
-                    allItems={allItems}
+        <div className='w-10/12 mx-auto mb-10'>
+            <div className='grid grid-cols-1 gap-4 lg:mx-8'>
+                {
+                    see_more.map(allItems => <AllItems
+                        key={allItems.unid}
+                        allItems={allItems}
 
-                ></AllItems>)
-            }
+                    ></AllItems>)
+                }
+            </div>
         </div>
     );
 };
